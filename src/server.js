a4afa366dd7ac971540a20e2f7b872c6ac57019d
Francisco Bressa <bressafrancisco@gmail.com>
2022-06-16 22:03:54 -0400

    getProducts

--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 import express from "express";
 import MailController from "./app/controllers/MailController"
+import axios from "axios";
 require("dotenv").config()
+const cors = require("cors")
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 app.get("/", MailController.store)
 
+app.get("/getProducts", async(req, res) => {
+    const { data } = await axios.get(
+        `https://vtexstore.codeby.com.br/api/catalog_system/pub/products/search`
+    );
+    res.json(data);
+})
 
-app.listen(3000, () => {
-    console.log('server is running on port 3000');
+
+app.listen(5000, () => {
+    console.log('server is running on port 5000');
 })
